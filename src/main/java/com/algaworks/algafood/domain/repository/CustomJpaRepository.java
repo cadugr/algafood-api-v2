f2e19f3e867355384a64cae630d34e275f424ab9
cadugr <cadu.guerra@gmail.com>
2020-01-27 22:55:48 -0300

    Estendendo o JpaRepository para customizar o repositório base

---
+++ src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
@@ -0,0 +1,14 @@
+package com.algaworks.algafood.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+//Com isso, estamos dizendo para o SDJ que não queremos que ele crie uma implementação do JpaRepository pra nós 
+@NoRepositoryBean
+public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID> {
+	
+	Optional<T> buscarPrimeiro();
+
+}
