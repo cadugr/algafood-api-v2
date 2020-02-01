96afe444ee453220af002a539bc44a79b84bb114
cadugr <cadu.guerra@gmail.com>
2020-02-01 18:26:53 -0300

    Adicionando dados de testes com callback do Flyway

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -61,7 +61,7 @@ public class Restaurante {
 	@Embedded
 	private Endereco endereco;
 	
-	//@JsonIgnore
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "restaurante_forma_pagamento", 
 			joinColumns = @JoinColumn(name = "restaurante_id"),
