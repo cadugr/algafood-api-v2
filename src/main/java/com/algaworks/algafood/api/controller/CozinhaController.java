0c07e999f874a0014d8e5d644d903bdfc9739cdd
cadugr <cadu.guerra@gmail.com>
2020-02-02 16:25:05 -0300

    Lancando excecoes customizadas anotadas com @ResponseStatus

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -17,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
@@ -70,18 +68,24 @@ public class CozinhaController {
 		
 	}
 	
+	//@DeleteMapping("/{cozinhaId}")
+	//public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
+		
+	//	try {		
+	//		cadastroCozinha.excluir(cozinhaId);
+	//		return ResponseEntity.noContent().build();
+	//	} catch (EntidadeNaoEncontradaException e) {
+	//		return ResponseEntity.notFound().build();
+	//	} catch (EntidadeEmUsoException e) {
+	//		return ResponseEntity.status(HttpStatus.CONFLICT).build();
+	//	}
+		
+	//}
+	
 	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId) {
-		
-		try {		
-			cadastroCozinha.excluir(cozinhaId);
-			return ResponseEntity.noContent().build();
-		} catch (EntidadeNaoEncontradaException e) {
-			return ResponseEntity.notFound().build();
-		} catch (EntidadeEmUsoException e) {
-			return ResponseEntity.status(HttpStatus.CONFLICT).build();
-		}
-		
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId) {
+		cadastroCozinha.excluir(cozinhaId);
 	}
 
 }
