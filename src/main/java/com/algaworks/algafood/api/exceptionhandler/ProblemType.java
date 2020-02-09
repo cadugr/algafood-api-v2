8d65f6cdcc0619c12a1568ddc299078f978b7589
cadugr <cadu.guerra@gmail.com>
2020-02-08 22:48:24 -0300

    Desafio: tratando exception de violacao de constraints de validacao

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	DADOS_IVALIDOS("/dados-invalidos","Dados inválidos"),
 	ERRO_DE_SISTEMA("/erro-de-sistema","Erro de sistema"),
 	PARAMETRO_INVALIDO("/parametro-ivalido","Parâmetro inválido"),
 	MENSAGEM_IMCOMPREENSIVEL("/mensagem-imcompreensivel", "Mensagem imcompreensível"),
