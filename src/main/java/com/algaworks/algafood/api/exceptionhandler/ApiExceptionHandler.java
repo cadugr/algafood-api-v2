685502ac96089eff8471dd66d8d20092a3cd7690
cadugr <cadu.guerra@gmail.com>
2020-02-11 22:00:47 -0300

    Customizando e resolvendo mensagens de validacao globais em Resource Bundle

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -6,6 +6,9 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -31,6 +34,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	private static final String MSG_ERRO_GENERICA_USUARIO_FINAL = "Ocorreu um erro inesperado no sistema. Tente novamente e se o "
 			+ "problema persistir, entre em contato com o administrador do sistema.";
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -38,10 +44,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		BindingResult result = ex.getBindingResult();
 		List<Problem.Field> fields = result.getFieldErrors().stream()
-		  .map(fieldError -> Problem.Field.builder()
+		  .map(fieldError -> { 
+			  	  String message = messageSource.getMessage(fieldError, LocaleContextHolder.getLocale());
+			  	  return Problem.Field.builder()
 				  .name(fieldError.getField())
-				  .userMessage(fieldError.getDefaultMessage())
-				  .build())
+				  .userMessage(message)
+				  .build();})
 		  		.collect(Collectors.toList());
 		
 		ProblemType problemType = ProblemType.DADOS_IVALIDOS;
