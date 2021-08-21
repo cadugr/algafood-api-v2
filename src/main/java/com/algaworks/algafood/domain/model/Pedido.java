452906787af3b5526844fe9a6bf4308c8da9f015
cadugr <cadu.guerra@gmail.com>
2021-08-21 17:22:52 -0300

    Usando IDs vs UUIDs nas URIs de recursos

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -18,6 +19,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -35,6 +37,8 @@ public class Pedido {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	private String codigo;
 	
 	private BigDecimal subtotal;
 	
@@ -101,12 +105,17 @@ public class Pedido {
 	private void setStatus(StatusPedido novoStatus) {
 		if(getStatus().naoPodeAlterarPara(novoStatus)) {
 			throw new NegocioException(
-					String.format("Status do pedido %d não pode ser alterado de %s para %s", getId(),
+					String.format("Status do pedido %s não pode ser alterado de %s para %s", getCodigo(),
 							getStatus().getDescricao(), 
 							novoStatus.getDescricao()));
 		}
 		
 		this.status = novoStatus;
+	}
+	
+	@PrePersist
+	private void gerarCodigo() {
+		setCodigo(UUID.randomUUID().toString());
 	}
 
 	
